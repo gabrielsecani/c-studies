3b5fb73cf85d25b3f415d01bc0f747e805092f3e
Gabriel Lucas <gabrielltr84@gmail.com>
2015-09-01 22:03:26 +0000

    alteração para usar "tipopilha.h"

--- pilha/pilhaencadeada.c
+++ pilha/pilhaencadeada.c
@@ -1,63 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-
-struct no{
-    int dado;
-    struct no *prox;
-    };
-
-typedef struct no *tipopilha;
-
-void init(tipopilha topo){
-    topo = NULL;
-}
-
-void push(tipopilha *topo, int elem){
-    tipopilha novo;
-    novo = (tipopilha)malloc(sizeof(struct no));
-    if(novo != NULL ){
-        novo->dado = elem;
-        novo->prox = *topo;
-        *topo = novo;
-    }else{
-        printf("Pilha está vazia.\n");
-    }
-}
-
-int isEmpty(tipopilha topo){
-    return (topo==NULL);
-    //0 = false
-    //1 = true
-}
-
-int pop(tipopilha *topo, int *elem){
-    tipopilha aux;
-    if(!isEmpty(*topo)){
-        aux = *topo;
-        *elem = aux->dado;
-        *topo = aux->prox;
-        free(aux);
-        return 1;
-    }else{
-        printf("Pilha está vazia.\n");
-        return 0;
-    }
-}
-
-int top(tipopilha *topo, int *elem){
-    if(!isEmpty(*topo)){
-        *elem = (*topo)->dado;
-        return 1;
-    }else{
-        printf("Pilha está vazia.\n");
-        return 0;
-    }
-}
+#include "tipopilha.h"
 
 int main(){
     tipopilha topo;
-    init(topo);
-    printf("%d", isEmpty(NULL));
+    init(&topo);
+    push(&topo, 2);
+    push(&topo, 5);
+
+    int itop = -1;
+
+    printf("\n%d",topA(topo, &itop));
+    printf("\ntop: %d", itop);
+    printf("\npop: %d", pop(&topo, &itop));
+    printf("\ntop: %d", itop);
+    printf("\n%d",top(&topo, &itop));
+    printf("\ntop: %d", itop);
+    printf("\nFIM.");
     getchar();
 }
